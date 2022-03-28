ffa534d8f1d0744a9ab96b1988983047e3351a04
viktoor48 <71779943+viktoor48@users.noreply.github.com>
2022-03-28 22:14:56 +0300

    завершил верстку балуна

--- balloon.js
+++ balloon.js
@@ -4,9 +4,16 @@ const name = document.querySelector('#name');
 const reviews = document.querySelector('#reviews');
 const input_impression = document.querySelector('#input-impression');
 const button_add = document.querySelector('#button-add');
+const close = document.querySelector('.close');
+
 
 button_add.addEventListener('click', function () {
     console.log(name.value);
     console.log(reviews.value);
     console.log(input_impression.value);
+    reviews_output.value += `${name.value} ${reviews.value}\n${input_impression.value}\n\n`;
+});
+
+close.addEventListener('click', function () {
+   document.querySelector('.balloon').classList.add('close-popup');
 });
--- ymap.js
+++ ymap.js
@@ -5,11 +5,30 @@ let placemarks = [
         latitude:52.60574955,
         longitude:39.60493108,
         hintContent: '<div class="map__hint">Нижний парк</div>',
-        balloonContent: [
-            '<div class="map__balloon">',
-            'Нижний парк',
-            '</div>'
-        ]
+        balloonContent: ['<div class="balloon">\n' +
+        '        <div class="header__balloon">\n' +
+        '            <div class="wrapper">\n' +
+        '                <div>\n' +
+        '                    <img class="geo" src="image/geo.png" alt="">\n' +
+        '                    <p class="head-address">Невский пр., 78, Санкт-Петербург, 191025</p>\n' +
+        '                </div>\n' +
+        '                <a href="#"><img class="close" src="image/close.png"></a>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '        <div class="main">\n' +
+        '            <textarea class="input-textarea output" id="reviews-output" readonly></textarea>\n' +
+        '            <div class="line"></div>\n' +
+        '            <div class="reviews">\n' +
+        '                <h3 class="orange-text">Ваш отзыв</h3>\n' +
+        '                <input class="input-text" id="name" placeholder="Ваше имя" type="text">\n' +
+        '                <input class="input-text" id="reviews" placeholder="Ваш отзыв" type="text">\n' +
+        '                <textarea class="input-textarea" id="input-impression" placeholder="Поделитесь впечатлениями" name=""></textarea>\n' +
+        '                <div class="btn-block">\n' +
+        '                    <button class="btn" id="button-add" >Добавить</button>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </div>']
     },
     {
         latitude:52.60550634,
@@ -63,3 +82,8 @@ function init() {
     map.geoObjects.add(clusterer);
     clusterer.add(geoObjects);
 }
+
+document.addEventListener('click', function (event) {
+   console.log(event);
+   console.log(event.target);
+});
